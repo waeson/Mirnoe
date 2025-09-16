c05d32e1cfe7f115f765ddc3da541e8eaefe731b
waeson <59735306+waeson@users.noreply.github.com>
2025-09-17 01:19:45 +0700

    Залил Мирное

---
+++ data.js
@@ -0,0 +1,468 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "МирНМ городок обзорная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7592620455687786,
+          "pitch": 0.31795684759393694,
+          "rotation": 3.141592653589793,
+          "target": "7--"
+        },
+        {
+          "yaw": -0.8516335037315592,
+          "pitch": 0.2589153348555655,
+          "rotation": 3.141592653589793,
+          "target": "2---"
+        },
+        {
+          "yaw": -2.358139372253774,
+          "pitch": 0.3567500362742333,
+          "rotation": 3.141592653589793,
+          "target": "1--"
+        },
+        {
+          "yaw": -2.2785879371326025,
+          "pitch": 0.3194984944041295,
+          "rotation": 3.141592653589793,
+          "target": "4--"
+        },
+        {
+          "yaw": -2.1124528408952408,
+          "pitch": 0.2634718187397347,
+          "rotation": 3.141592653589793,
+          "target": "6--"
+        },
+        {
+          "yaw": -1.5952839288373184,
+          "pitch": 0.2881650284757331,
+          "rotation": 3.141592653589793,
+          "target": "8--"
+        },
+        {
+          "yaw": -0.7857274907783562,
+          "pitch": 0.2979964816854981,
+          "rotation": 3.141592653589793,
+          "target": "9--"
+        },
+        {
+          "yaw": -1.9641070531504994,
+          "pitch": 0.18535842163299776,
+          "rotation": 3.141592653589793,
+          "target": "3--"
+        },
+        {
+          "yaw": -1.0044566180975867,
+          "pitch": 0.2110013621088136,
+          "rotation": 3.141592653589793,
+          "target": "5--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--",
+      "name": "МирНМ гардероб",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.4400372506680368,
+        "pitch": 0.25284604979193404,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.723694724294365,
+          "pitch": -0.115076286936171,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---",
+      "name": "МирНМ гостевой дом",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.2371836148307942,
+        "pitch": 0.16830762666651822,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.213015309763609,
+          "pitch": 0.03174064595690318,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "МирНМ парилка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.1463031487800315,
+        "pitch": 0.3845873185283075,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.266075191772309,
+          "pitch": 0.0905903596682478,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4--",
+      "name": "МирНМ прачечная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.071016603597151,
+        "pitch": 0.5046014439771671,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9029531061779892,
+          "pitch": 0.012788013300891521,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "МирНМ санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.7956477701797766,
+        "pitch": 0.33332927090500775,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.680788200081195,
+          "pitch": 0.03384959513615016,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--",
+      "name": "МирНМ сауна",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--",
+      "name": "МирНМ спортзал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.849804248954955,
+        "pitch": 0.23749651832178387,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.051078160363623226,
+          "pitch": -0.05195878850970459,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8--",
+      "name": "МирНМ столовая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 3.0797621358168126,
+        "pitch": 0.418767030210212,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1537792042365727,
+          "pitch": -0.017370926810793108,
+          "rotation": 5.497787143782138,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9--",
+      "name": "МирНМ туалет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.9280896020285105,
+        "pitch": 0.13555797776568923,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3438334229396656,
+          "pitch": -0.07104505696810648,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "МирНМ внутри",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
